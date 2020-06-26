99f25996bf0d579d005a4d5a21e5866255fcc1a4
rayquazacxj <48879507+rayquazacxj@users.noreply.github.com>
2020-06-26 23:07:40 +0800

    6267

--- fi/src/component/mindmapItem.js
+++ fi/src/component/mindmapItem.js
@@ -11,8 +11,10 @@ import Draggable from 'react-native-draggable'
 import { Button} from 'native-base';
 import {set_connect_candidate, update_connectSet} from '../state/mindmap-action'
 import AsyncStorage from '@react-native-community/async-storage';
+import {connect} from 'react-redux';
 
-export default class MindmapItem extends React.Component {
+
+class MindmapItem extends React.Component {
     constructor(props) {
         super(props)
         console.log(this.props)
@@ -23,6 +25,7 @@ export default class MindmapItem extends React.Component {
         }
         this.handleEdit = this.handleEdit.bind(this)
         this.handleSetConnect = this.handleSetConnect.bind(this)
+        this.handleDragRelease = this.handleDragRelease.bind(this)
         
     }
     render() {
@@ -30,7 +33,7 @@ export default class MindmapItem extends React.Component {
 
         return(
     
-            <Draggable x={50} y={100} disabled={this.state.disabled} onDrag={(evt)=>console.log('on drag',evt.nativeEvent.locationX)}>
+            <Draggable x={50} y={100} disabled={this.state.disabled} onDragRelease={(evt)=>this.handleDragRelease(evt)}>
                 <TouchableHighlight onPressIn={()=>this.setState({disabled: false })} underlayColor="green">
                     <View style={{padding:10},styles.buttont}>
                         <TextInput
@@ -52,6 +55,7 @@ export default class MindmapItem extends React.Component {
             id:this.props.mindmapItemNum
         })
         console.log('componentDidMount: ',this.state.id)*/
+        
     }
 
     handleSetConnect(){
@@ -71,16 +75,89 @@ export default class MindmapItem extends React.Component {
         return Math.floor(Math.random() * Math.floor(max-min));
     }
 
+    async handleDragRelease(evt){/*
+        let ItemData = await AsyncStorage.getItem(this.props.idx)
+        ItemData = ItemData ? JSON.parse(ItemData) : [] 
+        var lx = evt.nativeEvent.locationX
+        var ly = evt.nativeEvent.locationY
+
+        let newData = {
+            X: lx,
+            Y: ly,
+            text : (ItemData&&ItemData.text)? ItemData.text :""
+        }
+
+        console.log(ItemData,JSON.stringify(newData))
+        await  AsyncStorage.setItem(this.props.idx, JSON.stringify(newData))*/
+        /*
+        console.log('on drag',this.props.idx,evt.nativeEvent.locationX)
+        var lx = evt.nativeEvent.locationX
+        var ly =evt.nativeEvent.locationY
+
+        let ItemData = await AsyncStorage.getItem(this.props.idx)
+        ItemData = ItemData ? JSON.parse(ItemData) : []
+        console.log(result,ItemData)
+        ItemData.X = lx
+        ItemData.Y = ly
+        console.log(ItemData)
+        await AsyncStorage.setItem(this.props.idx, ItemData)
+*/
+        /*
+        AsyncStorage.getItem(this.props.idx).then((result,err) => {
+            var ItemData = result ? JSON.parse(result) : []
+            console.log(result,ItemData)
+            ItemData.X = lx
+            ItemData.Y = ly
+            console.log(ItemData)
+            AsyncStorage.setItem(this.props.idx, JSON.stringify(ItemData),err=>{
+                if(err)console.log('catch err')
+            })
+            console.log('async edited')
+        })*/
+
+    }
     
      
-    handleEdit = typedText => {
-        console.log("update => ", typedText);
-        this.setState({
-            text: typedText
-        });
+    async handleEdit(typedText){
+        try{
+            
+            console.log("update => ", typedText,this.props.idx);
+            this.setState({
+                text: typedText
+            });
 
-        AsyncStorage.getItem(this.props.idx,(err, result) => {
+            let ItemData = await AsyncStorage.getItem(this.props.idx)
+            ItemData = ItemData ? JSON.parse(ItemData) : [] 
+
+            let newData = {
+                X: (ItemData&&ItemData.X)?ItemData.X: 0,
+                Y: (ItemData&&ItemData.Y)?ItemData.Y: 0,
+                text : typedText
+            }
+
+            console.log(ItemData,JSON.stringify(newData))
+            await  AsyncStorage.setItem(this.props.idx, JSON.stringify(newData))
+            
+            let ItemData2 = await AsyncStorage.getItem(this.props.idx)
+            ItemData2 = ItemData2 ? JSON.parse(ItemData2) : [] 
+            console.log('i2: ',ItemData2)
+
+           
+        }catch(e){
+            if(e)console.log(e,'catch err')
+        }
+        
+
+        /*
+        let ItemData = await AsyncStorage.getItem(this.props.idx)
+        ItemData = ItemData ? JSON.parse(ItemData) : [] 
+        ItemData.text = typedText
+        console.log(ItemData)
+        await  AsyncStorage.setItem(this.props.idx, JSON.stringify(ItemData))*/
+        /*
+        AsyncStorage.getItem(this.props.idx).then((result,err) => {
             var ItemData = result ? JSON.parse(result) : []
+            console.log(result,ItemData)
             ItemData.text = typedText
             console.log(ItemData)
             AsyncStorage.setItem(this.props.idx, JSON.stringify(ItemData),err=>{
@@ -88,7 +165,7 @@ export default class MindmapItem extends React.Component {
             })
             console.log('async edited')
         })
-        /*
+        
         const storeData = async (typedText) => {
             try {
                 console.log('async edit')
@@ -133,9 +210,9 @@ const styles = StyleSheet.create({
 
     
 });
-/*
+
 export default connect(state => ({
     ...state.ConnectMindmapItem,
     mindmapItemNum: state.MindmapInfo.mindmapItemNum
     
-}))(MindmapItem);*/
+}))(MindmapItem);
