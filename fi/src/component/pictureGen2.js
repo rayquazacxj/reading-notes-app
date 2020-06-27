023accc8fd760683d5a4084b34c18a8d60b95646
rayquazacxj <48879507+rayquazacxj@users.noreply.github.com>
2020-06-27 23:26:17 +0800

    6279

--- fi/src/component/pictureGen2.js
+++ fi/src/component/pictureGen2.js
@@ -25,7 +25,9 @@ import { color } from 'react-native-reanimated';
           text: '',
           style:{
             fontSize:30
-          }
+          },
+          editor_invisible:false,
+          pictureSelector_invisible:false
 
       }
         
@@ -36,6 +38,7 @@ import { color } from 'react-native-reanimated';
         console.log('this.state.selectedPic: ',this.state.selectedPic)
         console.log('this.state.stlye: ',this.state.style)
 
+        var style_invisible =  false 
         //<Image source={require(this.state.selectedPic) }  style={{width:410,height:600}}></Image>
         return(
             <Container>
@@ -43,31 +46,36 @@ import { color } from 'react-native-reanimated';
                   
 
                   
-                  <ImageBackground source={this.state.selectedPic }  style={{width:411,height:600}}>
-                    <View>
+                  <ImageBackground source={this.state.selectedPic }  style={{width:411,height:650}}>
+                    <View style={{opacity: this.state.editor_invisible? 0:100}}>
                         <Item rounded  >
                             <TextInput style={this.state.style} placeholder="type here"  onChangeText={(text)=>this.setState({description: text})}/>
                         </Item>
+
                         <View style={{flexDirection:'row'}}>
                             <Button  small success onPress={()=>this.setState({style:{...this.state.style,fontWeight: 'bold',fontSize: 40}})} ><Text>  bold </Text></Button>
                             <Button  small success onPress={()=>this.setState({style:{...this.state.style,fontStyle: 'italic'}})} ><Text>  italic </Text></Button>
                             <Button  small success onPress={()=>this.setState({style:{...this.state.style,textDecorationLine: 'underline'}})} ><Text>  underline </Text></Button>
                             <Button  small success onPress={()=>this.setState({style:{...this.state.style,textDecorationLine: 'line-through'}})} ><Text>  line-through </Text></Button>
+                            
                         </View>
                     </View>
-
+                    <View style={{flexDirection:'row'}}>
+                        <Button  small success onPress={()=> this.setState({editor_invisible:!this.state.editor_invisible})} ><Text>  invisible </Text></Button>
+                        <Button  small success onPress={()=> this.setState({pictureSelector_invisible:!this.state.pictureSelector_invisible})} ><Text>  PICinvisible </Text></Button>
+                    </View>
                     <Draggable x={100} y={100}>
                         <Text style={this.state.style}>
                             {this.state.description}
                         </Text>  
                     </Draggable>
 
-                </ImageBackground>
+                
                       
                 
                     
 
-                        <View style={{flexDirection:'row'}}>
+                        <View style={{marginTop:367,flexDirection:'row',opacity: this.state.pictureSelector_invisible? 0:100}}>
                             <TouchableHighlight onPressIn={()=>this.handleSelectPic(require('../picture/PIC1.png'))} underlayColor="green">
                                 <Image source={require('../picture/PIC1.png') }  style={{width:150,height:150}}></Image>
                             </TouchableHighlight>
@@ -81,7 +89,7 @@ import { color } from 'react-native-reanimated';
                             </TouchableHighlight>
                         </View>
 
-                    
+                        </ImageBackground>
                     </Container> 
                                           
             
